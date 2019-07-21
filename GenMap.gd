23e7869f06537bd2f00f77f8a681f4153cc2117b
Michael Reel <mykilr@gmail.com>
2019-07-21 18:40:09 +0100

    Added another noise layer.

--- GenMap.gd
+++ GenMap.gd
@@ -3,7 +3,16 @@ extends Node2D
 # Config
 var base_default_tile_name = "Base Grass"
 var base_water_tile_name = "Base Water Auto"
+var base_seed = 1
+
+var ground_dirt_tile_name = "Dirt Auto"
+
+var noise_octaves := 3
+var noise_period := 20.0
+var noise_persistence := 0.8
+
 var base_land_threshold := -0.15
+var ground_threshold := 0.0
 
 # Reusable references
 var base_map : TileMap
@@ -12,7 +21,10 @@ var base_tileset : TileSet
 var ground_tileset: TileSet
 var base_default_tile : int
 var base_water_tile : int
+var ground_dirt_tile : int
+
 var base_noise : OpenSimplexNoise
+var ground_noise : OpenSimplexNoise
 
 func _ready():
 	setup_reusables()
@@ -25,13 +37,20 @@ func setup_reusables():
 	ground_tileset = ground_map.tile_set
 	base_default_tile = base_tileset.find_tile_by_name(base_default_tile_name)
 	base_water_tile = base_tileset.find_tile_by_name(base_water_tile_name)
+	ground_dirt_tile = ground_tileset.find_tile_by_name(ground_dirt_tile_name)
 	
 	# Configure noise
 	base_noise = OpenSimplexNoise.new()
-	base_noise.seed = 1
-	base_noise.octaves = 3
-	base_noise.period = 20.0
-	base_noise.persistence = 0.8
+	base_noise.seed = base_seed
+	base_noise.octaves = noise_octaves
+	base_noise.period = noise_period
+	base_noise.persistence = noise_persistence
+	
+	ground_noise = OpenSimplexNoise.new()
+	ground_noise.seed = base_seed + 1
+	ground_noise.octaves = noise_octaves
+	ground_noise.period = noise_period
+	ground_noise.persistence = noise_persistence
 
 func create_base_layer():
 	# Flood fill the tile map with the water tile to cover the screen size
@@ -40,9 +59,15 @@ func create_base_layer():
 			# Draw islands (using noise)
 			if base_noise.get_noise_2d(x, y) > base_land_threshold:
 				base_map.set_cell(x, y, base_default_tile)
+				
+				# Draw some dirt on top of the land
+				if ground_noise.get_noise_2d(x, y) > ground_threshold:
+					ground_map.set_cell(x, y, ground_dirt_tile)
+				
 				continue
 			# Default to water
 			base_map.set_cell(x, y, base_water_tile)
-	
+
 	# Call autotiling
 	base_map.update_bitmask_region()
+	ground_map.update_bitmask_region()
