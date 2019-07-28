50fa05d54412a7a4ffb55cdc812e710e040975d2
Michael Reel <mykilr@gmail.com>
2019-07-28 15:07:27 +0100

    Added simple animations to the player character.

--- player/Player.gd
+++ player/Player.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export(float) var speed = 5.0
+export(float) var speed = 2.0
 export(int) var tile_size = 16
 
 var axis_flip = false # This is to make diagonal movement 'slightly' less tedious
@@ -8,9 +8,11 @@ var axis_flip = false # This is to make diagonal movement 'slightly' less tediou
 func _process(delta):
 	var dir : Vector2 = get_input_dir()
 	if dir != Vector2.ZERO:
+		var anim_name : String = str(int(dir.x)) + "_" + str(int(dir.y))
 		var target : Vector2 = position + dir * tile_size
 		$Tween.interpolate_property(self, "position", position, target, 1.0 / speed, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		$Tween.start()
+		$AnimationPlayer.play(anim_name)
 
 func _on_Tween_tween_started(object, key):
 	set_process(false)
